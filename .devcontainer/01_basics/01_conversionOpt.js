1c568bd3ff2c027d4ec0d8f2b72dd7f471c0a79b
anushka0399 <156078392+anushka0399@users.noreply.github.com>
2025-06-23 14:22:27 +0000

    Add conversion operations and comparison examples to 01_conversionOpt.js

---
+++ .devcontainer/01_basics/01_conversionOpt.js
@@ -0,0 +1,48 @@
+
+let score = "33abc"
+//console.log(typeof(score));
+
+let value = Number(score)
+// console.log(typeof(value));
+// console.log(value); // NaN
+
+// ******* OPERATIONS ********
+
+let val = 3
+let negval = -val
+// console.log(negval);
+
+// console.log(1+"2");
+// console.log("2" + 1);
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2);
+
+// console.log(+true);
+
+// console.log(true+); //error
+
+let a = 100
+++a
+// console.log(a); // 101
+a++
+// console.log(a); // 101
+
+
+// ********COMPARISON*********
+
+console.log(null > 0);
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+// === it strictly checks the type and value
+
+console.log("2" == 2); // true
+
+
+
+
+
+
+
+
+
